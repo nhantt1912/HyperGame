76466f47c52d1ba345032edeabc997c20f246487
trannhan25 <trannhan2552@gmail.com>
2026-01-17 22:29:25 +0700

    - Add Animation Popup Win

--- Assets/Resources/Scripts/UI/Popup/WinPopup.cs
+++ Assets/Resources/Scripts/UI/Popup/WinPopup.cs
@@ -1,13 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class WinPopup : PopupBase
 {
+    [SerializeField] private RectTransform rectHand;
+    [SerializeField] private RectTransform rectText;
+    
     [SerializeField] private Button homeBTN;
     [SerializeField] private Button nextBTN;
+    
+    private Tween _scaleTween;
+    private Tween _rotateTween;
+    private Tween _scaleTweenText;
+    
     private void Start()
     {
         Debug.Log("Win Popup");
@@ -31,6 +40,30 @@ public class WinPopup : PopupBase
     private void OnLevelComplete(EventDefine.OnLevelComplete obj)
     {
         OnShow();
+    }
+
+    public override void OnShow()
+    {
+        base.OnShow();
+        AnimationOnWin();
+    }
+
+    private void AnimationOnWin()
+    {
+        KillAllTween();
+        _scaleTweenText = rectText.DOScale(Vector3.one * 1.2f, 0.8f).SetEase(Ease.OutBack);
+        _scaleTween = rectHand.DOScale(Vector3.zero, 0.8f).From().SetEase(Ease.OutBack);
+        _rotateTween = rectHand.DORotate(new Vector3(0, 0, 10), 0.2f)
+            .From(new Vector3(0, 0, -10))
+            .SetEase(Ease.InOutSine)
+            .SetLoops(10, LoopType.Yoyo);
+    }
+    
+    private void KillAllTween()
+    {
+        _scaleTween?.Kill();
+        _rotateTween?.Kill();
+        _scaleTweenText?.Kill();
     }
     
 }
