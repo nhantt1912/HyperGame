3f6a8e7643faab6c6db5de053355a7c7e7872f23
trannhan25 <trannhan2552@gmail.com>
2026-01-17 22:36:44 +0700

    - Add Scratch Card Package

---
+++ Assets/ScratchCard/Scripts/Editor/ScratchCardMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace ScratchCardAsset.Editor
+{
+    public class ScratchCardMenu : UnityEditor.Editor
+    {
+        private static string ScratchCardDefaultPath = "Assets/ScratchCard/Prefabs/ScratchCard.prefab";
+
+        private static void CreateScratchCard()
+        {
+            var asset = AssetDatabase.LoadAssetAtPath(ScratchCardDefaultPath, typeof(GameObject));
+            var instantiatedPrefab = PrefabUtility.InstantiatePrefab(asset);
+            Selection.activeObject = instantiatedPrefab;
+
+            EditorUtility.SetDirty(instantiatedPrefab);
+            EditorSceneManager.MarkSceneDirty(((GameObject) instantiatedPrefab).scene);
+        }
+    }
+}
